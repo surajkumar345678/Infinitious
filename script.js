9783bdff4af831a858fdf2429c1da759d859b9fa
Suraj Kumar <kumarsuraj345678@gmail.com>
2022-06-21 15:52:18 +0530

    added display photos functionality

--- script.js
+++ script.js
@@ -9,16 +9,27 @@ const count = 10;
 const apiKey = 'pfiusTi0rjDI73w0KchxNEfm38uui5s6VuoP1aEV11M';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
 function displayPhotos() {
     photosArray.forEach((photo) => {
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
 
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
         item.appendChild(img);
         imageContainer.appendChild(item);
 
