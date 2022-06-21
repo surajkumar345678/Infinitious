24d1ed40d7c6f0cb94f216f15fe7017bc3d90e58
Suraj Kumar <kumarsuraj345678@gmail.com>
2022-06-21 16:34:32 +0530

    added infinite scrolling finctionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
-
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 
@@ -9,6 +11,15 @@ const count = 10;
 const apiKey = 'pfiusTi0rjDI73w0KchxNEfm38uui5s6VuoP1aEV11M';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// image loader
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden=true;
+    }
+}
+
 function setAttributes(element, attributes) {
     for (const key in attributes) {
         element.setAttribute(key, attributes[key]);
@@ -16,25 +27,29 @@ function setAttributes(element, attributes) {
 }
 
 function displayPhotos() {
-    photosArray.forEach((photo) => {
-        const item = document.createElement('a');
+    imagesLoaded = 0;
 
-        setAttributes(item, {
-            href: photo.links.html,
-            target: '_blank',
-        });
-        const img = document.createElement('img');
+    totalImages = photosArray.length;
+    const item = document.createElement('a');
 
-        setAttributes(img, {
-            src: photo.urls.regular,
-            alt: photo.alt_description,
-            title: photo.alt_description,
-        });
-        item.appendChild(img);
-        imageContainer.appendChild(item);
-
+    setAttributes(item, {
+        href: photo.links.html,
+        target: '_blank',
     });
+    const img = document.createElement('img');
+
+    setAttributes(img, {
+        src: photo.urls.regular,
+        alt: photo.alt_description,
+        title: photo.alt_description,
+    });
+
+    img.addEventListener('load', imageLoaded)
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+
 }
+
 
 // getting photos from api
 async function getPhotos() {
@@ -49,11 +64,12 @@ async function getPhotos() {
 }
 
 // scroll function
-window.addEventListener('scroll',()=>{
-    if (window.innerHeight+window.scrollY>=document.body.offsetHeight-1000) {
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
         getPhotos();
     } else {
-        
+
     }
 });
 
